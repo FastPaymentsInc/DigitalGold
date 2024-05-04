61c9ea38d474062a28388b61bdb6fa89c5e277c8
lateminer <9951982+lateminer@users.noreply.github.com>
2024-05-05 01:04:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+USDG
+=====================================
+[![build](https://github.com/USDGcoinFoundation/USDGcoin/actions/workflows/build.yml/badge.svg?branch=main)](https://github.com/USDGcoinFoundation/USDGcoin/actions/workflows/build.yml)
+
+
+https://usdigitalgold.org
+
+USDG Wallet
+----------------
+
+US Digital Gold is an asset-backed private digital currency, coins in circulation will be backed in fiat currency and other conservative, stable assets.
+
+The NotFiat treasury is independently audited on a quarterly basis to ensure full transparency and accountability.
+
+USDG Wallet is the name of open source software which enables the use of this currency. This is experimental version of the software, based on Bitcoin Core 22.x.
---
+++ src/netmessagemaker.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_NETMESSAGEMAKER_H
+#define BITCOIN_NETMESSAGEMAKER_H
+
+#include <net.h>
+#include <serialize.h>
+
+class CNetMsgMaker
+{
+public:
+    explicit CNetMsgMaker(int nVersionIn) : nVersion(nVersionIn){}
+
+    template <typename... Args>
+    CSerializedNetMsg Make(int nFlags, std::string msg_type, Args&&... args) const
+    {
+        CSerializedNetMsg msg;
+        msg.m_type = std::move(msg_type);
+        // Blackcoin ToDo: revert after nodes upgrade to current version
+        int32_t serModes = nVersion <= OLD_VERSION ? SER_NETWORK : SER_NETWORK | SER_POSMARKER;
+        CVectorWriter{ serModes, nFlags | nVersion, msg.data, 0, std::forward<Args>(args)... };
+        return msg;
+    }
+
+    template <typename... Args>
+    CSerializedNetMsg Make(std::string msg_type, Args&&... args) const
+    {
+        return Make(0, std::move(msg_type), std::forward<Args>(args)...);
+    }
+
+    // Blackcoin ToDo: revert after nodes upgrade to current version
+    // /*
+    template <typename... Args>
+    CSerializedNetMsg MakeForSpecificClient(int nClientVersion, std::string msg_type, Args&&... args) const
+    {
+        int nFlags = 0;
+        CSerializedNetMsg msg;
+        msg.m_type = std::move(msg_type);
+        int32_t serModes = nClientVersion <= OLD_VERSION ? SER_NETWORK : SER_NETWORK | SER_POSMARKER;
+        CVectorWriter{ serModes, nFlags | nClientVersion, msg.data, 0, std::forward<Args>(args)... };
+        return msg;
+    }
+    // */
+
+private:
+    const int nVersion;
+};
+
+#endif // BITCOIN_NETMESSAGEMAKER_H
