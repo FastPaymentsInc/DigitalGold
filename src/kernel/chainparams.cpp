f347898837b3426c7822191412a0139169a71459
BlackcoinDev <dev@blackcoin.dev>
2024-09-23 20:27:45 +0200

    testnet: Activate SegWit on Sep 24, 2024

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -209,8 +209,8 @@ public:
         consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.fPowNoRetargeting = false;
         consensus.fPoSNoRetargeting = false;
-        consensus.nRuleChangeActivationThreshold = 11250; // 75% for testchains
-        consensus.nMinerConfirmationWindow = 15000; // nTargetTimespan / nTargetSpacing * 1000
+        consensus.nRuleChangeActivationThreshold = 1125; // 75% for testchains
+        consensus.nMinerConfirmationWindow = 1500; // nTargetTimespan / nTargetSpacing * 1000
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = Consensus::BIP9Deployment::NEVER_ACTIVE;
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
@@ -218,7 +218,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1727100000; // Monday, September 23, 2024 2:00:00 PM
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1727136000; // Monday, September 24, 2024 2:00:00 AM
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].min_activation_height = 0; // No activation delay
 
