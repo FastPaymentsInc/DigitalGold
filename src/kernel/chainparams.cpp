f23b53a6d2d972906346a1a73f10fb458eda905d
lateminer <9951982+lateminer@users.noreply.github.com>
2024-09-29 00:41:09 +0200

    consensus: Activate SegWit on Sep 30, 2024

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -109,7 +109,7 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = Consensus::BIP9Deployment::NEVER_ACTIVE;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1727719200; // Monday, September 30, 2024 6:00:00 PM
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].min_activation_height = 0; // No activation delay
 
